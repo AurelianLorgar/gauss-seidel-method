f099b594838bd6cf85fa3a98b37964308f0ec4f1
AurelianLorgar <dragon.priest.88@gmail.com>
2020-04-11 22:38:03 +0700

    Add new class and some things

--- src/sample/GraphicalAnalysis.java
+++ src/sample/GraphicalAnalysis.java
@@ -2,7 +2,6 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.BubbleChart;
-import javafx.scene.chart.NumberAxis;
 
 class GraphicalAnalysis {
 
@@ -11,17 +10,50 @@ class GraphicalAnalysis {
 
     void graphicalAnalysis(String targetFunctionNumberOne, String targetFunctionNumberTwo) {
 
-        int[] functionValue = {25, 50, 100, 200};
-
-        double[] y;
+        MathOperations mathOperations = new MathOperations();
 
         double numberOne = Integer.parseInt(targetFunctionNumberOne);
         double numberTwo = Integer.parseInt(targetFunctionNumberTwo);
 
-        /*double xPlus = 0.5*(2*numberOne+Math.sqrt(-3*(Math.pow(numberTwo, 2))+6*numberTwo*y[i]+4*functionValue[i]
-                - 3*(Math.pow(y[i], 2)))) + numberTwo - y;*/
+        int[] functionValue = {25, 50, 100, 200};
 
-        /*double xMinus = 0.5*(2*numberOne-Math.sqrt(-3*(Math.pow(numberTwo, 2))+6*numberTwo*y[i]+4*functionValue[i]
-                - 3*(Math.pow(y[i], 2)))) + numberTwo - y;*/
+        double[] y = {0, 2, -2, 4, -4, 6, -6, 8, -8, 10, -10, 12, -12, 14};
+
+        /*TODO: переделать массивы в листы, иначе ничего не выйдет, после чего сделать удаление одинаковых элементов и
+          TODO: засунуть всё это в BubbleChart*/
+        double[] xPlusFirst = new double[y.length];
+        double[] xMinusFirst = new double[y.length];
+
+        double[] xPlusSecond = new double[y.length];
+        double[] xMinusSecond = new double[y.length];
+
+        double[] xPlusThird = new double[y.length];
+        double[] xMinusThird = new double[y.length];
+
+        double[] xPlusFourth = new double[y.length];
+        double[] xMinusFourth = new double[y.length];
+
+        for (int i = 0; i < functionValue.length; i++) {
+            for (int j = 0; j < y.length; j++) {
+                switch (i) {
+                    case (0):
+                        xPlusFirst[j] = mathOperations.calculatePlus(y[j], numberOne, numberTwo, functionValue[i]);
+                        xMinusFirst[j] = mathOperations.calculateMinus(y[j], numberOne, numberTwo, functionValue[i]);
+                        break;
+                    case (1):
+                        xPlusSecond[j] = mathOperations.calculatePlus(y[j], numberOne, numberTwo, functionValue[i]);
+                        xMinusSecond[j] = mathOperations.calculateMinus(y[j], numberOne, numberTwo, functionValue[i]);
+                        break;
+                    case (2):
+                        xPlusThird[j] = mathOperations.calculatePlus(y[j], numberOne, numberTwo, functionValue[i]);
+                        xMinusThird[j] = mathOperations.calculateMinus(y[j], numberOne, numberTwo, functionValue[i]);
+                        break;
+                    case (3):
+                        xPlusFourth[j] = mathOperations.calculatePlus(y[j], numberOne, numberTwo, functionValue[i]);
+                        xMinusFourth[j] = mathOperations.calculateMinus(y[j], numberOne, numberTwo, functionValue[i]);
+                        break;
+                }
+            }
+        }
     }
 }
---
+++ src/sample/MathOperations.java
@@ -0,0 +1,74 @@
+package sample;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+class MathOperations {
+
+    double calculatePlus(double yAxis, double numOne, double numTwo, double value) {
+
+        double intermediateValue;
+        double yAxisPower;
+        double numTwoPower;
+        double plus;
+
+        yAxisPower = power(yAxis);
+        numTwoPower = power(numTwo);
+        intermediateValue = -3 * numTwoPower + 6 * numTwo * yAxis + 4 * value - 3 * yAxisPower;
+        intermediateValue = sqrt(intermediateValue);
+        if (intermediateValue == -1) {
+            return -1;
+        }
+        plus = 2 * numOne + intermediateValue;
+        plus = 0.5 * plus;
+        plus = plus + numTwo - yAxis;
+        plus = round(plus);
+        return plus;
+    }
+
+    double calculateMinus(double yAxis, double numOne, double numTwo, double value) {
+
+        double intermediateValue;
+        double yAxisPower;
+        double numTwoPower;
+        double minus;
+
+        yAxisPower = power(yAxis);
+        numTwoPower = power(numTwo);
+        intermediateValue = -3 * numTwoPower + 6 * numTwo * yAxis + 4 * value - 3 * yAxisPower;
+        intermediateValue = sqrt(intermediateValue);
+        if (intermediateValue == -1) {
+            return -1;
+        }
+        minus = 2 * numOne - intermediateValue;
+        minus = 0.5 * minus;
+        minus = minus + numTwo - yAxis;
+        minus = round(minus);
+        return minus;
+    }
+
+    private double power(double num) {
+        double result;
+
+        result = Math.pow(num, 2);
+        return result;
+    }
+
+    private double sqrt(double num) {
+        double result;
+
+        result = Math.sqrt(num);
+
+        if (Double.isNaN(result)) {
+            return -1;
+        } else {
+            return result;
+        }
+    }
+
+    private double round(double num) {
+        BigDecimal bd = new BigDecimal(Double.toString(num));
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+}
