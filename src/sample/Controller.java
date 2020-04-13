a15eb62e23536ae8bb9112b17051b9c82e44f045
AurelianLorgar <dragon.priest.88@gmail.com>
2020-04-13 12:21:15 +0700

    Little optimisation

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -23,6 +23,7 @@ public class Controller {
 
         String targetFunctionNumOne = textFieldTargetFunctionNumOne.getText();
         String targetFunctionNumTwo = textFieldTargetFunctionNumTwo.getText();
+        lineChart.setCreateSymbols(false);
 
         boolean result = new Check().checkSymbols(targetFunctionNumOne, targetFunctionNumTwo);
 
@@ -30,75 +31,27 @@ public class Controller {
             new Alerts().alertWrongSymbol();
         } else {
             graphicalAnalysis.graphicalAnalysis(targetFunctionNumOne, targetFunctionNumOne);
-            addToChart();
+            addToChart(graphicalAnalysis.xPlusFirst, graphicalAnalysis.xMinusFirst, graphicalAnalysis.yAxisFirst);
+            addToChart(graphicalAnalysis.xPlusSecond, graphicalAnalysis.xMinusSecond, graphicalAnalysis.yAxisSecond);
+            addToChart(graphicalAnalysis.xPlusThird, graphicalAnalysis.xMinusThird, graphicalAnalysis.yAxisThird);
+            addToChart(graphicalAnalysis.xPlusFourth, graphicalAnalysis.xMinusFourth, graphicalAnalysis.yAxisFourth);
         }
     }
 
-    //TODO: починить отображение и очистку поля
-    private void addToChart() {
+    //TODO: починить отображение и очистку поля и сделать так, чтобы разные графики были одного цвета и были соединены
+    private void addToChart(ArrayList<Double> xPlus, ArrayList<Double> xMinus, ArrayList<Double> yAxis) {
 
-        lineChart.setCreateSymbols(false);
+        XYChart.Series<Double, Double> seriesPlus = new XYChart.Series<>();
+        XYChart.Series<Double, Double> seriesMinus = new XYChart.Series<>();
 
-        XYChart.Series<Double, Double> series25Plus = new XYChart.Series<>();
-        XYChart.Series<Double, Double> series25Minus = new XYChart.Series<>();
-        XYChart.Series<Double, Double> series50Plus = new XYChart.Series<>();
-        XYChart.Series<Double, Double> series50Minus = new XYChart.Series<>();
-        XYChart.Series<Double, Double> series100Plus = new XYChart.Series<>();
-        XYChart.Series<Double, Double> series100Minus = new XYChart.Series<>();
-        XYChart.Series<Double, Double> series200Plus = new XYChart.Series<>();
-        XYChart.Series<Double, Double> series200Minus = new XYChart.Series<>();
+        seriesPlus.getData().clear();
+        seriesMinus.getData().clear();
 
-        series25Plus.getData().clear();
-        series25Minus.getData().clear();
-        series50Plus.getData().clear();
-        series50Minus.getData().clear();
-        series100Plus.getData().clear();
-        series100Minus.getData().clear();
-        series200Plus.getData().clear();
-        series200Minus.getData().clear();
-
-        ArrayList<Double> xPlusFirst = graphicalAnalysis.xPlusFirst;
-        ArrayList<Double> xMinusFirst = graphicalAnalysis.xMinusFirst;
-        ArrayList<Double> yAxisFirst = graphicalAnalysis.yAxisFirst;
-
-        ArrayList<Double> xPlusSecond = graphicalAnalysis.xPlusSecond;
-        ArrayList<Double> xMinusSecond = graphicalAnalysis.xMinusSecond;
-        ArrayList<Double> yAxisSecond = graphicalAnalysis.yAxisSecond;
-
-        ArrayList<Double> xPlusThird = graphicalAnalysis.xPlusThird;
-        ArrayList<Double> xMinusThird = graphicalAnalysis.xMinusThird;
-        ArrayList<Double> yAxisThird = graphicalAnalysis.yAxisThird;
-
-        ArrayList<Double> xPlusFourth = graphicalAnalysis.xPlusFourth;
-        ArrayList<Double> xMinusFourth = graphicalAnalysis.xMinusFourth;
-        ArrayList<Double> yAxisFourth = graphicalAnalysis.yAxisFourth;
-
-        for (int i = 0; i < yAxisFirst.size(); i++) {
-            series25Plus.getData().add(new XYChart.Data<>(xPlusFirst.get(i), yAxisFirst.get(i)));
-            series25Minus.getData().add(new XYChart.Data<>(xMinusFirst.get(i), yAxisFirst.get(i)));
+        for (int i = 0; i < yAxis.size(); i++) {
+            seriesPlus.getData().add(new XYChart.Data<>(xPlus.get(i), yAxis.get(i)));
+            seriesMinus.getData().add(new XYChart.Data<>(xMinus.get(i), yAxis.get(i)));
         }
-        lineChart.getData().add(series25Plus);
-        lineChart.getData().add(series25Minus);
-
-        for (int i = 0; i < yAxisSecond.size(); i++) {
-            series50Plus.getData().add(new XYChart.Data<>(xPlusSecond.get(i), yAxisSecond.get(i)));
-            series50Minus.getData().add(new XYChart.Data<>(xMinusSecond.get(i), yAxisSecond.get(i)));
-        }
-        lineChart.getData().add(series50Plus);
-        lineChart.getData().add(series50Minus);
-
-        for (int i = 0; i < yAxisThird.size(); i++) {
-            series100Plus.getData().add(new XYChart.Data<>(xPlusThird.get(i), yAxisThird.get(i)));
-            series100Minus.getData().add(new XYChart.Data<>(xMinusThird.get(i), yAxisThird.get(i)));
-        }
-        lineChart.getData().add(series100Plus);
-        lineChart.getData().add(series100Minus);
-
-        for (int i = 0; i < yAxisFourth.size(); i++) {
-            series200Plus.getData().add(new XYChart.Data<>(xPlusFourth.get(i), yAxisFourth.get(i)));
-            series200Minus.getData().add(new XYChart.Data<>(xMinusFourth.get(i), yAxisFourth.get(i)));
-        }
-        lineChart.getData().add(series200Plus);
-        lineChart.getData().add(series200Minus);
+        lineChart.getData().add(seriesPlus);
+        lineChart.getData().add(seriesMinus);
     }
 }
